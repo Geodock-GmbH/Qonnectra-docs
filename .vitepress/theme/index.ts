b3e5046e32b355aa0724612f3e5b2fa75706ccd4
Marius Heine <m.heine@geprog.com>
2026-03-25 09:46:56 +0100

    Replace some images and add lightbox (#13)
    * exchange header image
    
    * decrease opacity
    
    * only use map as header image
    
    * provide new images for home screen
    
    * add lightbox
    
    * add placeholder text for empty chapters

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import DefaultTheme, { VPButton } from 'vitepress/theme'
 import type { Theme } from 'vitepress'
+import Layout from './Layout.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component('VPButton', VPButton)
   },
--- manual/teil-b-betrieb-admin-qgis/14-arbeiten-mit-qgis.md
+++ manual/teil-b-betrieb-admin-qgis/14-arbeiten-mit-qgis.md
@@ -1,1 +1,3 @@
 # 14. Arbeiten mit QGIS im Kontext von Qonnectra
+
+_Die Dokumentation zu diesem Kapitel ist noch in Arbeit._
